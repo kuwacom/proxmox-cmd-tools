201134f4a81968c7e3457632d5d18b8f19e1a22e
kuwa <kuwa.com3@gmail.com>
2023-07-09 05:18:57 +0900

    add arg

--- pve-importdisk.sh
+++ pve-importdisk.sh
@@ -1,9 +1,9 @@
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
     echo not enough arguments.
-    echo "args... [vmID] [imagePath] [targetPool]"
+    echo "args... [vmID] [imagePath] [targetPool] [diskNum]"
     exit 1
 fi
 echo qm import Disk...
-qm importdisk $1 $2 $3
+qm importdisk $1 $2 $3 
 echo set Disk...
-qm set $1 --scsihw virtio-scsi-pci --scsi0 $3:vm-$1-disk-0
+qm set $1 --scsihw virtio-scsi-pci --scsi0 $3:vm-$1-disk-$4
